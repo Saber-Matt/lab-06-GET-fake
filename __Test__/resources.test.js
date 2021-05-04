69810e2b5961574b2c03d3bb0989cfc6296cd89b
Matthew Saber <mattsaber21@yahoo.com>
2021-05-03 17:39:43 -0700

    the tests shall pass

--- __Test__/resources.test.js
+++ __Test__/resources.test.js
@@ -5,26 +5,75 @@ const request = supertest(app);
 // If a GET reqest is made to /api/cats
 // does the server respond with status of 200
 // and body matching the cats data?
-it('GET /api/cats', async () => {
+test('GET cats', async () => {
+  const response = await request.get('/api/cats');
   const expected = [
     {
-      name: 'felix',
-      //...
+      name: 'Felix',
+      type: 'Tuxedo',
+      url: 'cats/felix.png',
+      year: 1892,
+      lives: 3,
+      isSidekick: false
     },
     {
-      name: 'garfield',
-      //...
+      name: 'Garfield',
+      type: 'Orange Tabby',
+      url: 'cats/garfield.jpeg',
+      year: 1978,
+      lives: 7,
+      isSidekick: false
+    },
+    {
+      name: 'Duchess',
+      type: 'Angora',
+      url: 'cats/duchess.jpeg',
+      year: 1970,
+      lives: 9,
+      isSidekick: false
+    },
+    {
+      name: 'Stimpy',
+      type: 'Manx',
+      url: 'cats/stimpy.jpeg',
+      year: 1990,
+      lives: 1,
+      isSidekick: true
+    },
+    {
+      name: 'Sylvester',
+      type: 'Tuxedo',
+      url: 'cats/sylvester.jpeg',
+      year: 1945,
+      lives: 1,
+      isSidekick: true
+    },
+    {
+      name: 'Tigger',
+      type: 'Orange Tabby',
+      url: 'cats/tigger.jpeg',
+      year: 1928,
+      lives: 8,
+      isSidekick: false
+    },
+    {
+      name: 'Hello Kitty',
+      type: 'Angora',
+      url: 'cats/hello-kitty.jpeg',
+      year: 1974,
+      lives: 9,
+      isSidekick: false
+    },
+    {
+      name: 'Hobbs',
+      type: 'Orange Tabby',
+      url: 'cats/hobbs.jpeg',
+      year: 1985,
+      lives: 6,
+      isSidekick: true
     }
   ];
 
-  const response = await request.get('/api/cats');
-
-  expect(response.status).toBe(200); // 200 = OK
+  expect(response.status).toBe(200);
   expect(response.body).toEqual(expected);
-
-});
-
-// API routes for cats
-app.get('/api/cats', (req, res) => {
-  res.json([]);
-});
+})
